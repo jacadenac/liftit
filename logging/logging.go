3725d156cfb97c49726516c285c0ef867f893075
jacadenac <jacadenac@unal.edu.co>
2017-05-01 18:54:03 -0500

    se agrega error en formato json

--- logging/logging.go
+++ logging/logging.go
@@ -3,7 +3,30 @@ package logging
 import (
 	"log"
 	"fmt"
+	"encoding/json"
 )
+
+type ServerError struct{
+	Error 	error	`json:"error"`
+	Detail 	string	`json:"detail"`
+}
+
+func (serverError ServerError)toJson(json_error []byte) {
+	json_error, err := json.Marshal(serverError)
+	FailOnError(err, "Error encoding ServerError struct")
+	return
+}
+
+func JsonError(err error, detail string)([]byte, bool){
+	json_error := []byte(`{}`)
+	if err != nil {
+		serverError := ServerError{err, detail}
+		json_error, err2 := json.Marshal(serverError)
+		FailOnError(err2, "Error encoding ServerError struct")
+		return json_error, true
+	}
+	return json_error, false
+}
 
 func FailOnError(err error, msg string) {
 	if err != nil {
